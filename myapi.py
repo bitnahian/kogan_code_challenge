62f4f6330145328c87174c89b9f38719c9cdd876
bitnahian <nhas9102@uni.sydney.edu.au>
2018-06-10 02:25:25 +1000

    finished

---
+++ myapi.py
@@ -0,0 +1,50 @@
+import requests 
+import json
+
+## Awesome API caller that can iterate over pages returned by the API call
+#
+#  Calculates the total volume for the given category in each page
+#  API_LINK : http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com/api/products/
+
+class API_Caller:
+
+    ## class variable API_LINK - The link for which the API will be called
+    API_LINK = "http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com/api/products/"
+
+    ## The constructor
+    #   
+    #  The default value of next_page is "1"
+    def __init__(self, category, next_page = "1"):
+        self.category = category
+        self.item_count = 0
+        self.next_page = "1"
+    
+    ## The iterator
+    def __iter__(self):
+        self.item_count = 0
+        self.next_page = "1"
+        return self
+
+    ## The next method
+    def __next__(self):
+        if self.next_page is not None:
+            return self.__callAPI()
+        else:
+            raise StopIteration
+
+    ## The __callAPI method is used to calculate the sum of the volume from the  next page
+    def __callAPI(self):
+        r = requests.get("{}{}".format(API_LINK, self.next_page))
+        contents = r.json() # Convert to JSON
+        objects = contents['objects']
+        # Set next_page to relevant value, None otherwise
+        self.next_page = contents['next'][-1] if contents['next'] is not None else None
+        volume_sum = 0
+        for item in objects:
+            if item['category'] == self.category:
+                self.item_count += 1
+                size = item['size']
+                # Calculate volume of item and add to sum
+                volume_sum += size['width']*size['length']*size['height']
+        return volume_sum
+
--- solution.py
+++ solution.py
@@ -1,40 +1,14 @@
-import requests 
-import json
+from myapi import API_Caller as apc
 
-## Awesome calculator that calucates the average volume of any category of
-#  products
-#  
-#  API_LINK : http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com/api/products/
-
-class API_Caller:
-
-    ## class variable API_LINK - The link for which the API will be called
-    API_LINK = http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com/api/products/
-
-    ## The constructor
-    def __init__(self, category):
-        self.category = category
-        self.page_count = 0
-        self.next_page = 1
-
-    def __iter__(self):
-        self.page_count = 0
-        self.next_page = 1
-        return self
-
-    def __next__(self):
-        self.sum += self.
-
-    def __callAPI(self):
-        r = requests.get("".format(API_LINK, self.next_page))
-        contents = r.json() # Convert to JSON
-        objects = contents['objects']
-        self.next_page = contents['next']
-        volume_sum = 0
-        for item in objects:
-            if item['category'] == self.category:
-                size = item['size']
-                # Calculate volume of item and add to sum
-                volume_sum += size['width']*size['length']*size['height']
+def calculate_average_volume(category, first_page = "1"):
+    iter_api = apc(category, first_page)
+    total_sum = 0
+    for vol in iter_api:
+        total_sum += vol 
+    average = total_sum/iter_api.item_count
+    return average
 
 
+if __name__ in "__main__":
+    
+    print("The average volume of all Air Conditioners is {:.3f}".format(calculate_average_volume("Air Conditioners")))
