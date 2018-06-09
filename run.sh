7cecf4c19c13f6f09162ad13dde3757ff632e4cd
bitnahian <nhas9102@uni.sydney.edu.au>
2018-06-10 03:36:37 +1000

    done

--- run.sh
+++ run.sh
@@ -1,10 +1,23 @@
 #! /usr/bin/bash
 
+printf "Starting the virtual environment.\n\n"
+
 # Activate virtualenv
 source env/bin/activate
 
+printf "Installing dependencies first.\n\n"
+# Install dependencies first
+pip install -r requirements.txt
+
+printf "Installation complete.\n\nRunning script.\n\n"
+
 # Run the solution
 python solution.py
+
+printf "\nMeasuring time taken to complete job.\n\n"
+
+time python solution.py
+
 
 # Deactivate
 deactivate
